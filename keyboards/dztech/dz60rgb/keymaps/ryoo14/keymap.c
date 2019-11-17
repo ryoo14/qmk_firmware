f01e4717ac11a95e5598318af76a24a702e0415c
ryoo14 <anana12185@gmail.com>
2019-11-17 16:33:13 +0900

    windows用にpauseキーを追加

--- keyboards/dztech/dz60rgb/keymaps/ryoo14/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/ryoo14/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |--------------------------------------------------------------------------------------|
     * | Z/H  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 | F8  | F9  | F10 | F11 | F12 |  DEL  |
     * |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+
-    * |RGBOFF|     |     |     |     |     |     |     |     |     |     |VOLUP|VOLDW|       |
+    * |RGBOFF|     |     |     |     |     |     |     |     |     |     |VOLUP|VOLDW| PAUSE |
     * |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+
     * |      |     |     |     |     |     |     |     |     |     |     |     |             |
     * |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     [_LAYER1] = LAYOUT( /* Base_FN */
       KC_GRV,    KC_F1,    KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,       KC_F9,   KC_F10,  KC_F11,     KC_F12,     KC_DEL ,    \
-      RGB_TOG,   TO(2),    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_VOLU,    KC_VOLD,    RESET  ,    \
+      RGB_TOG,   TO(2),    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_VOLU,    KC_VOLD,    KC_PAUS  ,    \
       KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,                EEP_RST,    \
       KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,          KC_TRNS,    KC_PGUP,    KC_TRNS,    \
       KC_TRNS,   KC_TRNS,  KC_TRNS,                       KC_TRNS,                                   KC_TRNS, KC_TRNS, KC_HOME,    KC_PGDOWN,  KC_END      \
